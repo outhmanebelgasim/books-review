c4cb5d9f13ffc1b4b81cb23284c725f6a0a8cc98
Belgasim Outhmane <125576314+outhmanebelgasim@users.noreply.github.com>
2026-03-01 16:58:57 +0000

    updates: adding to Book Model new functionalities (popular, highestRated, minReviews, dateFilter)

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
 
 class Book extends Model
 {
@@ -14,5 +17,42 @@ class Book extends Model
     ];
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title){
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null):Builder | QueryBuilder{
+        return $query
+        ->withCount([
+            "reviews" => function (Builder $q) use ($from, $to) {
+                $this->dateRangeFilter($q, $from, $to);
+            }
+        ])
+        ->orderBy("reviews_count", "desc");
+    }
+
+    public function scopehighestRated(Builder $query, $from = null, $to = null):Builder | QueryBuilder{
+        return $query
+        ->withAvg([
+            "reviews" => function (Builder $q) use ($from, $to) {
+                $this->dateRangeFilter($q, $from, $to);
+            }
+        ], "rating")
+            ->orderBy("reviews_avg_rating", "desc");
+    }
+
+    public function scopeminReviews(Builder $query, int $minReviews):Builder | QueryBuilder{
+        return $query->having("reviews_count", ">=", $minReviews);
+    }
+    private function dateRangeFilter(Builder $query, $from = null, $to = null){
+        if($from && !$to){
+                $query->where("created_at", ">=", $from);
+            }else if(!$from && $to){
+                $query->where("created_at", "<=", $to);
+            }else if($from && $to){
+                $query->whereBetween("created_at", [$from, $to]);
+            }
     }
 }
