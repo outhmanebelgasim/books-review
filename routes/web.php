6b2576cd13ac83cafe2375eb72ff2b6fc74c74f5
Belgasim Outhmane <125576314+outhmanebelgasim@users.noreply.github.com>
2026-03-02 02:44:42 +0000

    first creation: adding first route (resource)

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     dd();
 });
 
+Route::resource("books", BookController::class);
