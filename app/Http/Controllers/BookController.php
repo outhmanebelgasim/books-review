89c35facb6593a9e901d45903fdc29aa46c09e08
Belgasim Outhmane <125576314+outhmanebelgasim@users.noreply.github.com>
2026-03-03 23:37:33 +0000

    update: adding cache for show function

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,21 +16,20 @@ class BookController extends Controller
     $title = $request->input("title");
     $filter = $request->input("filter", "");
 
-    $query = Book::when($title,
+    $books = Book::when($title,
         fn($query, $title) => $query->title($title)
     );
 
-    $query = match($filter) {
-        "popular_last_month" => $query->popularLastMonth(),
-        "popular_last_6months" => $query->popularLast6Months(),
-        "highest_rated_last_month" => $query->highestRatedLastMonth(),
-        "highest_rated_last_6months" => $query->highestRated6LastMonths(),
-        default => $query->latest()
+    $books = match($filter) {
+        "popular_last_month" => $books->popularLastMonth(),
+        "popular_last_6months" => $books->popularLast6Months(),
+        "highest_rated_last_month" => $books->highestRatedLastMonth(),
+        "highest_rated_last_6months" => $books->highestRated6LastMonths(),
+        default => $books->latest()
     };
 
-    $cacheKey = "books:" . md5($filter . $title);
-
-    $books = Cache::remember($cacheKey, 3600, fn() => $query->get());
+    $cacheKey = "books:" . $filter . ":" . $title;
+    $books = Cache::remember($cacheKey, 3600, fn() => $books->get());
 
     return view("books.index", ["books" => $books]);
 }
@@ -56,10 +55,12 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view("books.show",
-        ["book" => $book->load([
-                "reviews" => fn ($query) =>$query->latest()
-            ])]);
+        $cacheKey = "book:" . $book->id;
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+            "reviews" => fn ($query) =>$query->latest()
+        ]));
+
+        return view("books.show",["book" => $book]);
     }
 
     /**
